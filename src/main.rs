d06f8a9c4a0c9ae901f803ccd52b5f2c82135948
Safe Gergis <safegerg@gmail.com>
2024-12-17 15:38:12 -0800

    window size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "JameGam2024".to_string(),
+                resolution: (640.0, 360.0).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
+
