2676a56d8a9d696638564d6873a7a5763a39dfe5
MacDudeDude <alexbrhls@gmail.com>
2024-12-17 20:50:00 -0800

    Added shadows and better sprite

--- src/enemy.rs
+++ src/enemy.rs
@@ -15,37 +15,65 @@ impl Plugin for EnemyPlugin {
 #[derive(Resource)]
 struct EnemyTimer(Timer);
 
-fn spawn_enemy(time: Res<Time>, mut timer: ResMut<EnemyTimer>, mut commands: Commands, asset_server: Res<AssetServer>) {
-    let circle = Circle ::new(200.0);
+fn spawn_enemy(
+    time: Res<Time>,
+    mut timer: ResMut<EnemyTimer>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    let circle = Circle::new(200.0);
     let boundary_pt = circle.sample_boundary(&mut rand::thread_rng());
     if timer.0.tick(time.delta()).just_finished() {
-        commands.spawn((
-            Sprite::from_image(asset_server.load("snowman.png")),
+        let num_offset = rand::thread_rng().gen_range(-1.0..1.0);      
+
+        let snowman_holder = commands.spawn((
+            Visibility::Visible,
             Transform::from_xyz(boundary_pt.x, boundary_pt.y, 2.0),
-            YSort{ z: 32.0, },
-            ChasePlayer{ speed: 25.0},
-            Wiggle{speed: 15.0, amount: 2.0, offset: time.elapsed_secs()},
-        ));
+            YSort { z: 32.0 },
+            ChasePlayer { speed: 25.0 },
+        )).id();
+
+        let snowman_sprite = commands.spawn((
+            Sprite::from_image(asset_server.load("Snowman2.png")),
+            Wiggle {
+                rotate_speed: 18.0,
+                rotate_amount: 0.02,
+                scale_speed: 18.0,
+                scale_amount: 0.15,
+                offset: num_offset,
+            },
+        )).id();
+
+        let snowman_shadow = commands.spawn((
+            Sprite::from_image(asset_server.load("Shadow.png")),
+            YSort { z: -100.0 },        
+        )).id();
+        commands.entity(snowman_holder).add_child(snowman_shadow);
+        commands.entity(snowman_holder).add_child(snowman_sprite);
     }
 }
 
 #[derive(Component)]
-struct Wiggle{
-    speed: f32,
-    amount: f32,
+struct Wiggle {
+    rotate_speed: f32,
+    rotate_amount: f32,
+    scale_speed: f32,
+    scale_amount: f32,
     offset: f32,
 }
 
 fn wiggle(time: Res<Time>, mut q: Query<(&mut Transform, &Wiggle)>) {
     for (mut tf, wiggle) in q.iter_mut() {
-        let sin_amount = f32::sin(time.elapsed_secs() * wiggle.speed);
-        let dt = time.delta_secs() * wiggle.amount as f32;
-        tf.rotate_z(sin_amount * dt);
+        let rotate_sin = f32::sin(wiggle.offset + time.elapsed_secs() * wiggle.rotate_speed);
+        let scale_sin = f32::sin(wiggle.offset + time.elapsed_secs() * wiggle.scale_speed);
+        let dt = time.delta_secs();
+        tf.rotate_z(rotate_sin * wiggle.rotate_amount);
+        tf.scale = Vec3::new(1.0 + scale_sin * wiggle.scale_amount, 1.0 - scale_sin * wiggle.scale_amount, 1.0);
     }
 }
 
 #[derive(Component)]
-struct ChasePlayer{
+struct ChasePlayer {
     speed: f32,
 }
 
@@ -63,9 +91,9 @@ struct YSort {
     z: f32,
 }
 
-fn y_sort(mut q: Query<(&mut Transform, &YSort)>,) {
+fn y_sort(mut q: Query<(&mut Transform, &YSort)>) {
     for (mut tf, ysort) in q.iter_mut() {
-        tf.translation.z = ysort.z -(1.0f32 / (1.0f32 + (2.0f32.powf(-0.01*tf.translation.y))));
+        tf.translation.z = ysort.z - (1.0f32 / (1.0f32 + (2.0f32.powf(-0.01 * tf.translation.y))));
     }
 }
 
